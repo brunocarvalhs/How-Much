2db76ea93d702386f981d74d331a291b6ec77ea1
Bruno Carvalho <bruno.silva.carvalho@outlook.com.br>
2025-09-29 18:03:06 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "br.com.brunocarvalhs.howmuch"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
