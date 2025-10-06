583eaaffbbebd4c7ed178061313e4927a8a1243a
Bruno <bruno.silva.carvalho@outlook.com.br>
2025-10-06 19:34:11 -0300

    feat: incluindo fluxo de criar lista de compra

--- data/src/main/java/br/com/brunocarvalhs/data/model/ProductModel.kt
+++ data/src/main/java/br/com/brunocarvalhs/data/model/ProductModel.kt
@@ -1,25 +1,33 @@
 package br.com.brunocarvalhs.data.model
 
-import br.com.brunocarvalhs.data.constants.EMPTY_LONG
 import br.com.brunocarvalhs.data.constants.EMPTY_STRING
 import br.com.brunocarvalhs.data.constants.ONE_INT
 import br.com.brunocarvalhs.domain.entities.Product
 import java.util.UUID
 import androidx.annotation.Keep
+import br.com.brunocarvalhs.data.constants.EMPTY_LONG
 
 @Keep
 data class ProductModel(
     override val id: String = UUID.randomUUID().toString(),
     override val name: String = EMPTY_STRING,
     override val price: Long = EMPTY_LONG,
-    override var quantity: Int = ONE_INT
+    override var quantity: Int = ONE_INT,
+    override var isChecked: Boolean = false
 ) : Product {
     override fun toCopy(
         id: String,
         name: String,
         price: Long,
-        quantity: Int
-    ): Product = copy(id = id, name = name, price = price, quantity = quantity)
+        quantity: Int,
+        isChecked: Boolean
+    ): Product = copy(
+        id = id,
+        name = name,
+        price = price,
+        quantity = quantity,
+        isChecked = isChecked
+    )
 }
 
 internal fun Product.toProductModel(): ProductModel =
@@ -27,5 +35,6 @@ internal fun Product.toProductModel(): ProductModel =
         id = this.id,
         name = this.name,
         price = this.price,
-        quantity = this.quantity
+        quantity = this.quantity,
+        isChecked = this.isChecked
     )
