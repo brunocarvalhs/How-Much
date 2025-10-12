ee3c4fd14cba86793f710c35562b1c4ac1361052
Bruno <bruno.silva.carvalho@outlook.com.br>
2025-10-12 17:19:06 -0300

    feat: refatorando input code

--- app/src/main/java/br/com/brunocarvalhs/howmuch/app/modules/token/TokenUiEffect.kt
+++
@@ -1,7 +0,0 @@
-package br.com.brunocarvalhs.howmuch.app.modules.token
-
-sealed interface TokenUiEffect {
-    data class ShowError(val message: String) : TokenUiEffect
-    data class NavigateToAddProduct(val cartId: String?) : TokenUiEffect
-    data class ShareCart(val shareText: String) : TokenUiEffect
-}
