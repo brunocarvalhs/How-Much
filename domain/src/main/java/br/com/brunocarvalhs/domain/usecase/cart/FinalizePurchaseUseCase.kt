736855ba769e06bebd7166ee880eadbd9d737ddc
Bruno <bruno.silva.carvalho@outlook.com.br>
2025-10-12 20:20:33 -0300

    feat: novo usecases

--- domain/src/main/java/br/com/brunocarvalhs/domain/usecase/cart/FinalizePurchaseUseCase.kt
+++ domain/src/main/java/br/com/brunocarvalhs/domain/usecase/cart/FinalizePurchaseUseCase.kt
@@ -7,11 +7,12 @@ class FinalizePurchaseUseCase @Inject constructor(
     private val repository: ShoppingCartRepository,
     private val createNewCartUseCase: CreateNewCartUseCase
 ) {
-    suspend operator fun invoke(cartId: String, market: String, price: Long): Result<Unit> =
+    suspend operator fun invoke(cartId: String?, market: String, price: Long): Result<Unit> =
         runCatching {
+            if (cartId == null) return@runCatching
             val cart = repository.findById(cartId) ?: return@runCatching
             val finalizedCart = cart.finalizePurchase(market, price)
             repository.update(finalizedCart)
-            createNewCartUseCase() // Cria um novo carrinho vazio
+            createNewCartUseCase().getOrThrow()
         }
 }
