120e659802bbad15887d959a3d23170161dc653f
bruno <bruno.silva.carvalho@outlook.com.br>
2026-09-01 22:49:50 -0300

    fix: import ordering in CestouBottomNavigation (Detekt)

--- core/ui/src/main/java/br/com/brunocarvalhs/howmuch/core/ui/components/CestouBottomNavigation.kt
+++ core/ui/src/main/java/br/com/brunocarvalhs/howmuch/core/ui/components/CestouBottomNavigation.kt
@@ -3,7 +3,6 @@ package br.com.brunocarvalhs.howmuch.core.ui.components
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.AutoAwesome
@@ -19,13 +18,14 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import br.com.brunocarvalhs.howmuch.core.navigation.ShoppingList
 import br.com.brunocarvalhs.howmuch.core.navigation.mobile.AiChat
 import br.com.brunocarvalhs.howmuch.core.navigation.mobile.Profile
-import br.com.brunocarvalhs.howmuch.core.navigation.ShoppingList
 import br.com.brunocarvalhs.howmuch.core.ui.R
 import coil.compose.AsyncImage
 
